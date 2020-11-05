ac155fd228e83a9a12e9aedfc450795e4906aee7
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-05 10:42:36 -0500

    created moveX function and added callback

--- callback_hell.js
+++ callback_hell.js
@@ -1,6 +1,20 @@
 const btn = document.querySelector('button');
 
-//translate wrapped by a callback function that executes every second
+//translate wrapped by a callback function that executes every second. added another setTimout so every second the button moves 100 pixels
 setTimeout(() => {
 	btn.style.transform = 'translateX(100px)';
+	setTimeout(()=> {
+		btn.style.transform = 'translateX(200px)';
+	},2000);
 },1000);
+
+
+const moveX = (element,amount,delay,callback) => {
+	setTimeout(()=>{
+		element.style.transform = 'translateX(${amount}px)';
+		if (callback) callback();
+	},delay);
+};
+
+moveX(btn,100,1000,()=>{moveX(btn,200,1000);
+});
