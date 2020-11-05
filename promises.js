f575ba0a1d35122561fbf0bb3101a1ab705c411f
Siddesh <siddesh@Siddeshs-MacBook-Pro.local>
2020-11-05 11:10:03 -0500

    added .then and .catch methods

--- promises.js
+++ promises.js
@@ -1,3 +1,20 @@
 const willGetYouADog = new Promise((resolve,reject)=>{
-	//reject()
+	const rand = Math.random();
+	if(rand<0.5){
+		resolve();
+	}
+	else{
+		reject();
+	}
 });
+
+//every promise has a .then method while will run if a promise is resolved. promises also have a .catch method which will run if rejected
+
+willGetYouADog.then(()=> {
+	console.log("YAY WE GOT A DOG");
+});
+
+willGetYouADog.catch(()=>{
+	console.log("No dog");
+});
+
